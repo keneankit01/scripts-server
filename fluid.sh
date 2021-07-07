4ea5e80861cac3ab289f246d8dd55019171e6337
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-07-07 15:41:31 +0530

    Create fluid.sh

---
+++ fluid.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export GITHUB_USER="keneankit01"
+export GITHUB_EMAIL="keneankit01@gmail.com"
+
+export device="DRG"
+
+export ROM="Fluid OS"
+export ROM_DIR="${WORKSPACE}/work/rom"
+export ROM_VERSION="11"
+export official="false"
+export local_manifest_url=""
+export manifest_url="https://github.com/Project-Fluid/manifest.git"
+export rom_vendor_name="fluid"
+export branch="fluid-11"
+export bacon="bacon"
+export buildtype=""
+export clean="clean"
+export generate_incremental="false"
+export upload_recovery=""
+
+export ccache="true"
+export ccache_size="30"
+
+export jenkins="true"
+
+export release_repo="keneankit01/scripts-server"
+
+export timezone="UTC"
