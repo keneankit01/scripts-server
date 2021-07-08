7c2588431c021603f54b9175fa0bab64c2a20ea1
BolaRamy <82784605+bola980@users.noreply.github.com>
2021-07-08 14:26:22 +0200

    Create Spark.sh

---
+++ Spark.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export GITHUB_USER="keneankit01"
+export GITHUB_EMAIL="keneankit01@gmail.com"
+
+export device="SWEET"
+
+export ROM="Spark OS"
+export ROM_DIR="${WORKSPACE}/work/rom"
+export ROM_VERSION="11"
+export official="true"
+export manifest_url="https://github.com/Spark-Rom/manifest"
+export rom_vendor_name="spark"
+export branch="fire"
+export bacon="spark"
+export buildtype="userdebug"
+export clean="clean"
+export generate_incremental="false"
+export upload_recovery=""
+
+export ccache="true"
+export ccache_size="30"
+
+export jenkins="true"
+
+export release_repo="keneankit01/scripts-server"
+
+export timezone="UTC"
