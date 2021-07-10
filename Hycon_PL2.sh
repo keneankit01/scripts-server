947eed00babf9b268398b29a98687a0572f21803
ANKIT KENE <keneankit01@gmail.com>
2021-07-10 10:45:46 +0530

    Hycon

---
+++ Hycon_PL2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export GITHUB_USER="keneankit01"
+export GITHUB_EMAIL="keneankit01@gmail.com"
+
+export device="DRG"
+
+export ROM="HyconOS"
+export ROM_DIR="${WORKSPACE}/work/hycon"
+export ROM_VERSION="V4.0"
+export official="false"
+export local_manifest_url="https://raw.githubusercontent.com/keneankit01/android_.repo_local_manifests/lineage-18.1/local_manifest.xml"
+export manifest_url="https://github.com/HyconOS/manifest.git"
+export rom_vendor_name="aosp"
+export branch="eleven"
+export bacon="bacon"
+export buildtype="userdebug"
+export clean="clean"
+export generate_incremental="false"
+export upload_recovery="true"
+
+export ccache="true"
+export ccache_size="30"
+
+export jenkins="true"
+
+export release_repo="HyconOS-Releases/PL2/releases"
+
+export timezone="UTC"
+
+
